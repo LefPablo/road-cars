afdcd7be869bc42004cb2440d8085d97295817cb
Pavel <finskiypavel@gmail.com>
2019-11-03 01:26:51 +0300

    added DB singleton class

---
+++ src/main/java/DataBase.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public final class DataBase {
+    private static DataBase db;
+    private Connection connection;
+    private Statement statement;
+
+    private DataBase () {
+        //get Driver class
+        String DB_Driver = "org.h2.Driver";
+        try {
+            Class.forName(DB_Driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //connect to db
+        try {   //getConnection("jdbc:h2:./test", "sa", " "); local db
+            connection = DriverManager.getConnection("jdbc:h2:mem:test");
+            statement = connection.createStatement();
+            creatTable();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    private void creatTable() throws SQLException {
+        statement.execute("CREATE TABLE IF NOT EXISTS TEST (ID INT PRIMARY KEY AUTO_INCREMENT, CARNUMBER VARCHAR(16), TIMESTAMP VARCHAR(40))");
+    }
+
+    public static synchronized DataBase getDb() {
+        if (db == null) {
+            synchronized (DataBase.class) {
+                if (db == null)
+                    db = new DataBase();
+            }
+        }
+        return db;
+    }
+}
---
+++ src/test/java/DataBaseTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataBaseTest {
+
+    @Test
+    public void getDb() {
+
+
+        System.out.println(DataBase.getDb());
+        System.out.println(DataBase.getDb());
+    }
+}
